5049cfe14975f1f879157ce53508a213b3fc48a8
thomation <thomation@126.com>
2022-01-15 11:52:28 +0800

    tune the coordinate

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,11 @@ fn main() {
     let image_height = 256;
     let channel = 3;
     let mut pixels = vec![0; image_width * image_height * channel];
-    for h in 0..image_height {
+    for h in 0..image_height{
         for w in 0..image_width {
             let r = w as f32 / (image_width - 1) as f32;
-            let g = h as f32 / (image_height - 1) as f32;
+            let g = (image_height - h - 1) as f32 / (image_height - 1) as f32;
             let b = 0.25;
-            // pixels[w + h * image_width + 0] = ;
-            // pixels[w + h * image_width + 1] = ;
-            // pixels[w + h * image_width + 2] = ;
             pixels[w * channel + h * image_width * channel] = (r * 255.999) as u8;
             pixels[1 + w * channel + h * image_width * channel] = (g * 255.999) as u8;
             pixels[2 + w * channel + h * image_width * channel] = (b * 255.999) as u8;
