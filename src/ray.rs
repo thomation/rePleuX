2327e6b25d4844d63ead7814cebc84265ee48eef
thomation <thomation@126.com>
2022-02-05 09:15:46 +0800

    [Refactor] Add Dir3 type

--- src/ray.rs
+++ src/ray.rs
@@ -1,10 +1,10 @@
 use crate::vector;
 pub struct Ray {
     origin: vector::Point3,
-    dir: vector::Vec3,
+    dir: vector::Dir3,
 }
 impl Ray {
-    pub fn new(origin: vector::Point3, dir: vector::Vec3) -> Ray {
+    pub fn new(origin: vector::Point3, dir: vector::Dir3) -> Ray {
         Ray {
             origin: origin,
             dir: dir,
@@ -13,7 +13,7 @@ impl Ray {
     pub fn origin(&self) -> vector::Point3 {
         self.origin.clone()
     }
-    pub fn dir(&self) -> vector::Vec3 {
+    pub fn dir(&self) -> vector::Dir3 {
         self.dir.clone()
     }
     pub fn at(&self, t: f64) -> vector::Point3 {
--- src/vector.rs
+++ src/vector.rs
@@ -4,6 +4,7 @@ pub struct Vec3 {
 }
 pub type Color3 = Vec3;
 pub type Point3 = Vec3;
+pub type Dir3 = Vec3;
 
 impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
