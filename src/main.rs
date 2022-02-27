b36c73cb40f0939c1de959dd0bc32c6468c8481d
thomation <thomation@126.com>
2022-02-27 21:01:12 +0800

    super sample

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use crate::hit::Hittable;
-mod ray;
-mod vector;
-mod hit;
-mod sphere;
-mod hittable_list;
+use rand::Rng;
 mod camera;
+mod hit;
+mod hittable_list;
 mod output;
+mod ray;
+mod sphere;
+mod vector;
 
 fn ray_color(ray: &ray::Ray, hit: Option<hit::HitRecord>) -> vector::Color3 {
     match hit {
         Option::Some(r) => {
-            return vector::Color3::new(r.normal().x() + 1.0, r.normal().y() + 1.0, r.normal().z() + 1.0) * 0.5;
-        },
+            return vector::Color3::new(
+                r.normal().x() + 1.0,
+                r.normal().y() + 1.0,
+                r.normal().z() + 1.0,
+            ) * 0.5;
+        }
         Option::None => {
             let unit = vector::Vec3::unit(&ray.dir());
             let t = (unit.y() + 1.0) * 0.5;
-            return vector::Color3::new(0.5, 0.7, 1.0) * t + vector::Color3::new(1.0, 1.0, 1.0) * (1.0 - t)
+            return vector::Color3::new(0.5, 0.7, 1.0) * t
+                + vector::Color3::new(1.0, 1.0, 1.0) * (1.0 - t);
         }
     }
 }
@@ -29,19 +35,32 @@ fn main() {
     let cam = camera::Camera::new(aspect_ratio);
     // Scene
     let mut world = hittable_list::HittableList::new();
-    world.add(Box::new(sphere::Sphere::new(vector::Point3::new(0.0, 0.0, -1.0), 0.5)));
-    world.add(Box::new(sphere::Sphere::new(vector::Point3::new(0.0, -100.5, -1.0), 100.0)));
+    world.add(Box::new(sphere::Sphere::new(
+        vector::Point3::new(0.0, 0.0, -1.0),
+        0.5,
+    )));
+    world.add(Box::new(sphere::Sphere::new(
+        vector::Point3::new(0.0, -100.5, -1.0),
+        100.0,
+    )));
     // Render
     let image_width = img.width();
     let image_height = img.height();
+    let samples_per_pixels = 100;
+    let mut rng = rand::thread_rng();
     for h in 0..image_height {
         for w in 0..image_width {
-            // Screen Coordinate
-            let u = w as f64 / (image_width as f64 - 1.0);
-            let v = (image_height - h - 1) as f64 / (image_height as f64 - 1.0);
-            let ray = cam.get_ray(u, v);
-            let hit = world.hit(&ray, 0.1, 10.0);
-            let color = ray_color(&ray, hit);
+            let mut color = vector::Color3::new(0.0, 0.0, 0.0);
+            for _ in 0..samples_per_pixels {
+                let rw:f64 = rng.gen();
+                let rh:f64 = rng.gen();
+                let u = (w as f64 + rw ) / (image_width as f64 - 1.0);
+                let v = ((image_height - h - 1) as f64  + rh)/ (image_height as f64 - 1.0);
+                let ray = cam.get_ray(u, v);
+                let hit = world.hit(&ray, 0.1, 10.0);
+                color += ray_color(&ray, hit);
+            }
+            color /= samples_per_pixels as f64;
             img.write_color(w, h, &color);
         }
     }
