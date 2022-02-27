4622081b27dc7439d967ba21387c3046980abd43
thomation <thomation@126.com>
2022-02-27 19:27:55 +0800

    Extract camera class

---
+++ src/camera.rs
@@ -0,0 +1,38 @@
+use crate::ray;
+use crate::vector;
+
+pub struct Camera {
+    origin: vector::Point3,
+    lower_left_corner_world: vector::Point3,
+    horizontal: vector::Point3,
+    vertical: vector::Point3,
+}
+
+impl Camera {
+    pub fn new(aspect_ratio: f64) -> Camera {
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * aspect_ratio;
+        let focal_lenght = 1.0;
+        let origin = vector::Point3::new(0.0, 0.0, 0.0);
+        let horizontal = vector::Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = vector::Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner_view = -horizontal.clone() / 2.0
+            - vertical.clone() / 2.0
+            - vector::Vec3::new(0.0, 0.0, focal_lenght);
+        let lower_left_corner_world = origin.clone() + lower_left_corner_view;
+        Camera {
+            origin: origin,
+            lower_left_corner_world: lower_left_corner_world,
+            horizontal: horizontal,
+            vertical: vertical,
+        }
+    }
+    pub fn get_ray(&self, u: f64, v: f64) -> ray::Ray {
+        let p = self.lower_left_corner_world.clone()
+            + self.horizontal.clone() * u
+            + self.vertical.clone() * v;
+
+        let dir = p - self.origin.clone();
+        ray::Ray::new(self.origin.clone(), dir)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod vector;
 mod hit;
 mod sphere;
 mod hittable_list;
+mod camera;
 
 fn ray_color(ray: &ray::Ray, hit: Option<hit::HitRecord>) -> vector::Color3 {
     match hit {
@@ -24,20 +25,11 @@ fn ray_color(ray: &ray::Ray, hit: Option<hit::HitRecord>) -> vector::Color3 {
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * aspect_ratio;
-    let focal_lenght = 1.0;
-    let origin = vector::Point3::new(0.0, 0.0, 0.0);
-    let horizontal = vector::Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = vector::Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner_view = -horizontal.clone() / 2.0
-        - vertical.clone() / 2.0
-        - vector::Vec3::new(0.0, 0.0, focal_lenght);
-    let lower_left_corner_world = origin.clone() + lower_left_corner_view;
+    let cam = camera::Camera::new(aspect_ratio);
     // Scene
     let mut world = hittable_list::HittableList::new();
     world.add(Box::new(sphere::Sphere::new(vector::Point3::new(0.0, 0.0, -1.0), 0.5)));
@@ -51,10 +43,7 @@ fn main() {
             let u = w as f64 / (image_width as f64 - 1.0);
             let v = (image_height - h - 1) as f64 / (image_height as f64 - 1.0);
             // World Coordinate
-            let p = lower_left_corner_world.clone() + horizontal.clone() * u + vertical.clone() * v;
-
-            let dir = p - origin.clone();
-            let ray = ray::Ray::new(origin.clone(), dir);
+            let ray = cam.get_ray(u, v);
             let hit = world.hit(&ray, 0.1, 10.0);
             let color = ray_color(&ray, hit);
             pixels[w * channel + h * image_width * channel] = (color.x() * 255.999) as u8;
