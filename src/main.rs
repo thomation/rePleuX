39e9aa3a88d0e6b7acddf2fd1b851471b3a6fcae
thomation <thomation@126.com>
2022-03-07 16:18:10 +0800

    [Refactor] Extract render class.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use crate::hit::Hittable;
-use rand::Rng;
 mod camera;
 mod hit;
 mod hittable_list;
@@ -7,32 +6,15 @@ mod output;
 mod ray;
 mod sphere;
 mod math;
+mod render;
 
-fn ray_color(ray: &ray::Ray, hit: Option<hit::HitRecord>) -> math::vector::Color3 {
-    match hit {
-        Option::Some(r) => {
-            return math::vector::Color3::new(
-                r.normal().x() + 1.0,
-                r.normal().y() + 1.0,
-                r.normal().z() + 1.0,
-            ) * 0.5;
-        }
-        Option::None => {
-            let unit = math::vector::Vec3::unit(&ray.dir());
-            let t = (unit.y() + 1.0) * 0.5;
-            return math::vector::Color3::new(0.5, 0.7, 1.0) * t
-                + math::vector::Color3::new(1.0, 1.0, 1.0) * (1.0 - t);
-        }
-    }
-}
 
 fn main() {
-    // Image
+    // Bitmap
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
     let mut img = output::bitmap::Bitmap::new(image_width, image_height);
-    let img_file = output::picture::Png::new("target/output.png".to_string());
     // Camera
     let cam = camera::Camera::new(aspect_ratio);
     // Scene
@@ -46,23 +28,8 @@ fn main() {
         100.0,
     )));
     // Render
-    let samples_per_pixels = 10;
-    let mut rng = rand::thread_rng();
-    for h in 0..image_height {
-        for w in 0..image_width {
-            let mut color = math::vector::Color3::new(0.0, 0.0, 0.0);
-            for _ in 0..samples_per_pixels {
-                let rw: f64 = rng.gen();
-                let rh: f64 = rng.gen();
-                let u = (w as f64 + rw) / (image_width as f64 - 1.0);
-                let v = ((image_height - h - 1) as f64 + rh) / (image_height as f64 - 1.0);
-                let ray = cam.get_ray(u, v);
-                let hit = world.hit(&ray, 0.1, 10.0);
-                color += ray_color(&ray, hit);
-            }
-            color /= samples_per_pixels as f64;
-            img.write_color(w, h, &color);
-        }
-    }
+    render::RayTracing::render(image_width, image_height, 1, &cam, &world, &mut img);
+    // Output
+    let img_file = output::picture::Png::new("target/output.png".to_string());
     img_file.save(&img);
 }
---
+++ src/render.rs
@@ -0,0 +1,53 @@
+use crate::camera::Camera;
+use crate::hit;
+use crate::math;
+use crate::output;
+use crate::ray;
+use rand::Rng;
+
+pub struct RayTracing {}
+impl RayTracing {
+    pub fn render(
+        image_width: usize,
+        image_height: usize,
+        samples_per_pixels: usize,
+        cam: &Camera,
+        hitable: &hit::Hittable,
+        output: &mut output::bitmap::Bitmap,
+    ) {
+        let mut rng = rand::thread_rng();
+        for h in 0..image_height {
+            for w in 0..image_width {
+                let mut color = math::vector::Color3::new(0.0, 0.0, 0.0);
+                for _ in 0..samples_per_pixels {
+                    let rw: f64 = rng.gen();
+                    let rh: f64 = rng.gen();
+                    let u = (w as f64 + rw) / (image_width as f64 - 1.0);
+                    let v = ((image_height - h - 1) as f64 + rh) / (image_height as f64 - 1.0);
+                    let ray = cam.get_ray(u, v);
+                    let hit = hitable.hit(&ray, 0.1, 10.0);
+                    color += RayTracing::ray_color(&ray, hit);
+                }
+                color /= samples_per_pixels as f64;
+                output.write_color(w, h, &color);
+            }
+        }
+    }
+    fn ray_color(ray: &ray::Ray, hit: Option<hit::HitRecord>) -> math::vector::Color3 {
+        match hit {
+            Option::Some(r) => {
+                return math::vector::Color3::new(
+                    r.normal().x() + 1.0,
+                    r.normal().y() + 1.0,
+                    r.normal().z() + 1.0,
+                ) * 0.5;
+            }
+            Option::None => {
+                let unit = math::vector::Vec3::unit(&ray.dir());
+                let t = (unit.y() + 1.0) * 0.5;
+                return math::vector::Color3::new(0.5, 0.7, 1.0) * t
+                    + math::vector::Color3::new(1.0, 1.0, 1.0) * (1.0 - t);
+            }
+        }
+    }
+}
