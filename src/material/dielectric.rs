867a9eaf2f17971a007714e75cdf9b17427c7a52
thomation <thomation@126.com>
2022-06-05 10:27:34 +0800

    Add branch of refract and reflect

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,14 +1,14 @@
 use super::material;
-use crate::math;
-use crate::hit;
 use super::scatter;
+use crate::hit;
+use crate::math;
 pub struct Dielectric {
-    index_of_fraction : f64,
+    index_of_fraction: f64,
 }
 impl Dielectric {
-    pub fn new (index_of_fraction: f64) -> Dielectric {
+    pub fn new(index_of_fraction: f64) -> Dielectric {
         Dielectric {
-            index_of_fraction : index_of_fraction,
+            index_of_fraction: index_of_fraction,
         }
     }
 }
@@ -18,9 +18,29 @@ impl material::Material for Dielectric {
         ray_in: &math::ray::Ray,
         hit_record: &hit::record::HitRecord,
     ) -> std::option::Option<scatter::ScatterResult> {
-        let refraction_ratio = if hit_record.front_face() {1.0 / self.index_of_fraction} else {self.index_of_fraction};
+        let refraction_ratio = if hit_record.front_face() {
+            1.0 / self.index_of_fraction
+        } else {
+            self.index_of_fraction
+        };
         let unit_dir = math::vector::Vec3::unit(&ray_in.dir());
-        let refracted = math::vector::Vec3::refract(&unit_dir, &hit_record.normal(), refraction_ratio);
-        Option::Some(scatter::ScatterResult::new(math::ray::Ray::new(hit_record.position(), refracted), math::vector::Color3::new(1.0, 1.0, 1.0)))
+        let cos_theta =
+            math::vector::Vec3::dot(&math::vector::Vec3::neg(&unit_dir), &hit_record.normal())
+                .min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        if refraction_ratio * sin_theta > 1.0 {
+            let reflected = math::vector::Vec3::reflect(&unit_dir, &hit_record.normal());
+            Option::Some(scatter::ScatterResult::new(
+                math::ray::Ray::new(hit_record.position(), reflected),
+                math::vector::Color3::new(1.0, 1.0, 1.0),
+            ))
+        } else {
+            let refracted =
+                math::vector::Vec3::refract(&unit_dir, &hit_record.normal(), refraction_ratio);
+            Option::Some(scatter::ScatterResult::new(
+                math::ray::Ray::new(hit_record.position(), refracted),
+                math::vector::Color3::new(1.0, 1.0, 1.0),
+            ))
+        }
     }
 }
