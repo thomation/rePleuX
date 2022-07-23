5ae8305aa845ad25d1f1c67d63fea66fde9e34fe
thomation <thomation@126.com>
2022-07-23 09:29:30 +0800

    Add radom scene

--- src/scene.rs
+++ src/scene.rs
@@ -17,6 +17,45 @@ impl Scene {
         )));
 
         // Random spheres
+        let point = math::vector::Point3::new(4.0, 0.2, 0.0);
+        for a in -11..11 {
+            for b in -11..11 {
+                let choose_mat = math::random::generate();
+                let center = math::vector::Point3::new(
+                    a as f64 + 0.9 * math::random::generate(),
+                    0.2,
+                    b as f64 + math::random::generate(),
+                );
+                if (center.clone() - &point).length() > 0.9 {
+                    if choose_mat < 0.8 {
+                        // diffuse
+                        let albedo =
+                            math::vector::Color3::random() * math::vector::Color3::random();
+                        objects.push(Box::new(sphere::Sphere::new(
+                            center,
+                            0.2,
+                            material::lambertian::Lambertian::new(albedo),
+                        )));
+                    } else if choose_mat < 0.95 {
+                        // metal
+                        let albedo = math::vector::Color3::random_range(0.5, 1.0);
+                        let fuzz = math::random::generate_range(0.0, 0.5);
+                        objects.push(Box::new(sphere::Sphere::new(
+                            center,
+                            0.2,
+                            material::metal::Metal::new(albedo, fuzz),
+                        )));
+                    } else {
+                        // glass
+                        objects.push(Box::new(sphere::Sphere::new(
+                            center,
+                            0.2,
+                            material::dielectric::Dielectric::new(1.5),
+                        )));
+                    }
+                }
+            }
+        }
 
         objects.push(Box::new(sphere::Sphere::new(
             math::vector::Point3::new(0.0, 1.0, 0.0),
