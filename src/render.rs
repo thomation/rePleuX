5c1410c1929363b20b4b3df6a1d51b4e88cb82d1
thomation <thomation@126.com>
2022-07-23 10:47:29 +0800

    Print progress

--- src/render.rs
+++ src/render.rs
@@ -4,6 +4,7 @@ use crate::math;
 use crate::math::ray;
 use crate::output;
 use crate::scene;
+use std::time::Instant;
 
 pub struct RayTracing {}
 impl RayTracing {
@@ -16,7 +17,9 @@ impl RayTracing {
         world: &scene::Scene,
         output: &mut output::bitmap::Bitmap,
     ) {
+        let all_time = Instant::now();
         for h in 0..image_height {
+            let row_time = Instant::now();
             for w in 0..image_width {
                 let mut color = math::vector::Color3::new(0.0, 0.0, 0.0);
                 for _ in 0..samples_per_pixels {
@@ -30,6 +33,12 @@ impl RayTracing {
                 color /= samples_per_pixels as f64;
                 output.write_color(w, h, &color);
             }
+            println!(
+                "{}% Finished, Raw time: {} secs All time: {}",
+                (h + 1) as f64 / image_height as f64 * 100.0,
+                row_time.elapsed().as_secs(),
+                all_time.elapsed().as_secs()
+            );
         }
     }
     fn ray_color(ray: &ray::Ray, world: &scene::Scene, depth: usize) -> math::vector::Color3 {
@@ -42,7 +51,8 @@ impl RayTracing {
                 let scatter = rec.material().scatter(&ray, &rec);
                 match scatter {
                     Option::Some(sr) => {
-                        return RayTracing::ray_color(sr.ray(), &world, depth - 1) * sr.attenuation();
+                        return RayTracing::ray_color(sr.ray(), &world, depth - 1)
+                            * sr.attenuation();
                     }
                     Option::None => {
                         return math::vector::Color3::new(0.0, 0.0, 0.0);
