4ca75f60e9b01f7f8fa8cb54dc6e48a54530b086
thomation <thomation@126.com>
2022-10-05 10:47:13 +0800

    800

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
 
     let world = scene::scene::Scene::new();
     let aspect_ratio = world.camera().aspect_ratio();
-    let image_width = 600;
+    let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
     let img = Arc::new(Mutex::new(io::bitmap::Bitmap::new(image_width, image_height)));
     render::RayTracing::render(
