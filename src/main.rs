42ddf3cfda3e53a3c649d08de8792669a60e4566
thomation <thomation@126.com>
2022-10-22 09:25:36 +0800

    hemisphere sample

--- src/main.rs
+++ src/main.rs
@@ -12,38 +12,26 @@ use std::sync::Mutex;
 use std::time::Instant;
 
 fn main() {
-    // let now = Instant::now();
-    // let samples_per_pixel = 128;
-    // let max_depth = 50;
+    let now = Instant::now();
+    let samples_per_pixel = 100;
+    let max_depth = 50;
 
-    // let world = scene::scene::Scene::new();
-    // let aspect_ratio = world.camera().aspect_ratio();
-    // let image_width = 800;
-    // let image_height = (image_width as f64 / aspect_ratio) as usize;
-    // let img = Arc::new(Mutex::new(io::bitmap::Bitmap::new(image_width, image_height)));
-    // render::RayTracing::render2(
-    //     image_width,
-    //     image_height,
-    //     samples_per_pixel,
-    //     max_depth
-    //     6,
-    //     Arc::new(world),
-    //     img.clone()
-    // );
-    // let img_file = io::picture::Png::new("target/output.png".to_string());
-    // img_file.save(img);
-    // println!("Run time: {} secs", now.elapsed().as_secs());
+    let world = scene::scene::Scene::new();
+    let aspect_ratio = world.camera().aspect_ratio();
+    let image_width = 600;
+    let image_height = (image_width as f64 / aspect_ratio) as usize;
+    let img = Arc::new(Mutex::new(io::bitmap::Bitmap::new(image_width, image_height)));
+    render::RayTracing::render2(
+        image_width,
+        image_height,
+        samples_per_pixel,
+        max_depth,
+        6,
+        Arc::new(world),
+        img.clone()
+    );
+    let img_file = io::picture::Png::new("target/output.png".to_string());
+    img_file.save(img);
+    println!("Run time: {} secs", now.elapsed().as_secs());
 
-    // Test mc
-    const N: usize = 10000;
-    let mut sum = 0.0;
-    for _ in 0..N {
-        let d = material::material::random_in_unit_vector();
-        let cos2 = d.z() * d.z();
-        sum += cos2 / pdf(&d);
-    }
-    println!("I = {}", sum / N as f64);
-}
-fn pdf(p: &math::vector::Vec3) -> f64 {
-    0.25 / 3.14
 }
--- src/material/lambertian.rs
+++ src/material/lambertian.rs
@@ -1,5 +1,6 @@
 use super::material;
 use super::scatter;
+use crate::hit;
 use crate::hit::record::HitRecord;
 use crate::math;
 use crate::math::vector;
@@ -23,17 +24,15 @@ impl<T: texturable::Texturable> material::Material for Lambertian<T> {
         ray_in: &math::ray::Ray,
         hit_record: &HitRecord,
     ) -> Option<scatter::ScatterResult> {
-        let mut scatter_dir = hit_record.normal().clone() + material::random_in_unit_vector();
-        if scatter_dir.near_zero() {
-            scatter_dir = hit_record.normal().clone();
-        }
+        let mut scatter_dir = material::random_in_half_sphere(hit_record.normal());
+        scatter_dir.normalize();
         let ray_out =
             math::ray::Ray::new(hit_record.position().clone(), scatter_dir.clone(), ray_in.time());
         Option::Some(scatter::ScatterResult::new(
             ray_out,
             self.albedo()
                 .value(hit_record.u(), hit_record.v(), hit_record.position()),
-            vector::Vec3::dot(&hit_record.normal(), &scatter_dir) / std::f64::consts::PI,
+            0.5 / std::f64::consts::PI,
         ))
     }
     fn emitted(&self, u: f64, v: f64, p: &math::vector::Point3) -> math::vector::Color3 {
