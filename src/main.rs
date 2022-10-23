646c2001a5a7d2d0e852b006b50fbecbd8ea8f9f
thomation <thomation@126.com>
2022-10-23 09:07:56 +0800

    test random consine dir

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,44 @@ use std::sync::Mutex;
 use std::time::Instant;
 
 fn main() {
-    let now = Instant::now();
-    let samples_per_pixel = 100;
-    let max_depth = 50;
+    // let now = Instant::now();
+    // let samples_per_pixel = 100;
+    // let max_depth = 50;
 
-    let world = scene::scene::Scene::new();
-    let aspect_ratio = world.camera().aspect_ratio();
-    let image_width = 600;
-    let image_height = (image_width as f64 / aspect_ratio) as usize;
-    let img = Arc::new(Mutex::new(io::bitmap::Bitmap::new(image_width, image_height)));
-    render::RayTracing::render2(
-        image_width,
-        image_height,
-        samples_per_pixel,
-        max_depth,
-        6,
-        Arc::new(world),
-        img.clone()
-    );
-    let img_file = io::picture::Png::new("target/output.png".to_string());
-    img_file.save(img);
-    println!("Run time: {} secs", now.elapsed().as_secs());
+    // let world = scene::scene::Scene::new();
+    // let aspect_ratio = world.camera().aspect_ratio();
+    // let image_width = 600;
+    // let image_height = (image_width as f64 / aspect_ratio) as usize;
+    // let img = Arc::new(Mutex::new(io::bitmap::Bitmap::new(image_width, image_height)));
+    // render::RayTracing::render2(
+    //     image_width,
+    //     image_height,
+    //     samples_per_pixel,
+    //     max_depth,
+    //     6,
+    //     Arc::new(world),
+    //     img.clone()
+    // );
+    // let img_file = io::picture::Png::new("target/output.png".to_string());
+    // img_file.save(img);
+    // println!("Run time: {} secs", now.elapsed().as_secs());
+    
+    let N = 1000000;
+    let mut sum = 0.0;
+    for _ in 0..N {
+        let v = random_cosine_direction();
+        sum += v.z() * v.z() * v.z() / (v.z() / std::f64::consts::PI);
+    }
+    println!("pi / 2 = {}", std::f64::consts::PI / 2.0);
+    println!("estimate = {}", sum / N as f64);
+}
 
+fn random_cosine_direction() -> math::vector::Dir3 {
+    let r1 = math::random::generate();
+    let r2 = math::random::generate();
+    let z = (1.0 - r2).sqrt();
+    let phi = 2.0 * std::f64::consts::PI * r1;
+    let x = phi.cos() * r2.sqrt();
+    let y = phi.sin() * r2.sqrt();
+    math::vector::Dir3::new(x, y, z)
 }
